fed6de52a22e62ca3490aa6b1d185ee32c772189
Yossa Bourne <cybercanonx@gmail.com>
2024-07-09 23:51:35 +0700

    chore: init sequelize migrations

--- src/sequelize/sequelize.ts
+++ src/sequelize/sequelize.ts
@@ -1,17 +1,17 @@
 import { Sequelize } from 'sequelize'
 
-// const POSTGRES_USERS_DB_NAME = process.env.POSTGRES_USERS_DB_NAME || ''
-// const POSTGRES_USER = process.env.POSTGRES_USER || ''
-// const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD || ''
-// const POSTGRES_HOST = process.env.POSTGRES_HOST || ''
-// const POSTGRES_PORT = Number(process.env.POSTGRES_PORT)
-// 
-// const sequelize = new Sequelize(POSTGRES_USERS_DB_NAME, POSTGRES_USER, POSTGRES_PASSWORD, {
-//   host: POSTGRES_HOST,
-//   port: POSTGRES_PORT,
-//   dialect: 'postgres',
-// })
+const POSTGRES_USERS_DB_NAME = process.env.POSTGRES_USERS_DB_NAME || 'postgres'
+const POSTGRES_USER = process.env.POSTGRES_USER || 'postgres'
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD || '1qaz2wsx3edc'
+const POSTGRES_HOST = process.env.POSTGRES_HOST || '35.240.255.177'
+const POSTGRES_PORT = Number(process.env.POSTGRES_PORT || 5432)
 
-const sequelize = new Sequelize('sqlite::memory:')
+const sequelize = new Sequelize(POSTGRES_USERS_DB_NAME, POSTGRES_USER, POSTGRES_PASSWORD, {
+  host: POSTGRES_HOST,
+  port: POSTGRES_PORT,
+  dialect: 'postgres',
+})
+
+// const sequelize = new Sequelize('sqlite::memory:')
 
 export default sequelize
