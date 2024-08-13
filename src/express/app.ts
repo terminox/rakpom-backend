c455cc5f55e3a727cfe850f6b7292e7906c2b23e
Yossa Bourne <cybercanonx@gmail.com>
2024-08-13 23:14:09 +0700

    chore: add hello dummy route

--- src/express/app.ts
+++ src/express/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response } from 'express'
 import morgan from 'morgan'
 
 import userRoutes from './users/users.router'
@@ -13,5 +13,8 @@ app.use(express.json())
 
 app.use('/api/v1/users', userRoutes)
 app.use('/api/v1/shops', shopRoutes)
+app.use('/api/v1/hello', (req: Request, res: Response) => {
+  res.send('Hello World')
+})
 
 export default app
