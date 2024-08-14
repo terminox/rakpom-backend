5dc5ab99fb834bd30e4a8437afe66cc793cea8ec
Yossa Bourne <cybercanonx@gmail.com>
2024-08-15 00:25:19 +0700

    chore: add Sequelize migrations and Cloud SQL proxy

--- src/sequelize/models/shop_coordinates_item.ts
+++ src/sequelize/models/shop_coordinates_item.ts
@@ -1,41 +1,41 @@
-import { Model, DataTypes, InferAttributes, InferCreationAttributes, CreationOptional, ModelDefined } from 'sequelize'
+// import { Model, DataTypes, InferAttributes, InferCreationAttributes, CreationOptional, ModelDefined } from 'sequelize'
 
-import sequelize from '..'
+// import sequelize from '..'
 
-import Shop from './shop'
+// import Shop from './shop'
 
-class ShopCoordinatesItem extends Model<InferAttributes<ShopCoordinatesItem>, InferCreationAttributes<ShopCoordinatesItem>> {
-  declare id: string
-  declare shopID: string
-  declare latitude: number
-  declare longitude: number
-}
+// class ShopCoordinatesItem extends Model<InferAttributes<ShopCoordinatesItem>, InferCreationAttributes<ShopCoordinatesItem>> {
+//   declare id: string
+//   declare shopID: string
+//   declare latitude: number
+//   declare longitude: number
+// }
 
-ShopCoordinatesItem.init({
-  id: {
-    type: DataTypes.STRING,
-    primaryKey: true,
-    unique: true,
-  },
-  shopID: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    references: {
-      model: Shop,
-      key: 'id',
-    }
-  },
-  latitude: {
-    type: DataTypes.DOUBLE,
-    allowNull: false,
-  },
-  longitude: {
-    type: DataTypes.DOUBLE,
-    allowNull: false,
-  },
-}, {
-  sequelize,
-  modelName: 'ShopCoordinatesItem',
-})
+// ShopCoordinatesItem.init({
+//   id: {
+//     type: DataTypes.STRING,
+//     primaryKey: true,
+//     unique: true,
+//   },
+//   shopID: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//     references: {
+//       model: Shop,
+//       key: 'id',
+//     }
+//   },
+//   latitude: {
+//     type: DataTypes.DOUBLE,
+//     allowNull: false,
+//   },
+//   longitude: {
+//     type: DataTypes.DOUBLE,
+//     allowNull: false,
+//   },
+// }, {
+//   sequelize,
+//   modelName: 'ShopCoordinatesItem',
+// })
 
-export default ShopCoordinatesItem
+// export default ShopCoordinatesItem
