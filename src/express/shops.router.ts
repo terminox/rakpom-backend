d5a305a72e5b8a0cbce7426c15e630d003942d17
Yossa Bourne <cybercanonx@gmail.com>
2024-11-17 16:44:31 +0700

    fix(shop): fix shop reviews

--- src/express/shops.router.ts
+++ src/express/shops.router.ts
@@ -21,6 +21,7 @@ import SequelizeBalanceInfoService from './shops/balance_info/balance_info_servi
 import SequelizeWithdrawRequestCreationService from './shops/withdraw_request/withdraw_request_creation_service.sequelize'
 import SequelizeWithdrawRequestFetchingService from './shops/withdraw_request/withdraw_request_fetching_service.sequelize'
 import SequelizeTransactionFetchingService from './shops/transactions/transaction_fetching_service.sequelize'
+import SequelizeShopReviewsFetchingService from './users/shop_reviews/shop_reviews_fetching_service.sequelize'
 
 const router = Router()
 
@@ -131,50 +132,15 @@ router.get('/withdraw-history-items', shopAuth, (req: Request, res: Response) =>
   // TODO
 })
 
-router.get('/reviews', shopAuth, (req: Request, res: Response) => {
-  // TODO
-  res.status(200).json(response([
-    {
-      id: '1',
-      reviewerName: 'Alice',
-      score: 5,
-      text: 'Good',
-      dateString: '2021-01-01',
-      reviewerImageURL: 'https://plus.unsplash.com/premium_photo-1671656349218-5218444643d8?q=80&w=3486&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    },
-    {
-      id: '2',
-      reviewerName: 'Bob',
-      score: 4,
-      text: 'Not bad',
-      dateString: '2021-01-02',
-      reviewerImageURL: 'https://plus.unsplash.com/premium_photo-1671656349218-5218444643d8?q=80&w=3486&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    },
-    {
-      id: '3',
-      reviewerName: 'Charlie',
-      score: 3,
-      text: 'So so',
-      dateString: '2021-01-03',
-      reviewerImageURL: 'https://plus.unsplash.com/premium_photo-1671656349218-5218444643d8?q=80&w=3486&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    },
-    {
-      id: '4',
-      reviewerName: 'David',
-      score: 2,
-      text: 'Bad',
-      dateString: '2021-01-04',
-      reviewerImageURL: 'https://plus.unsplash.com/premium_photo-1671656349218-5218444643d8?q=80&w=3486&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    },
-    {
-      id: '5',
-      reviewerName: 'Eve',
-      score: 1,
-      text: 'Very bad',
-      dateString: '2021-01-05',
-      reviewerImageURL: 'https://plus.unsplash.com/premium_photo-1671656349218-5218444643d8?q=80&w=3486&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    }
-  ]))
+router.get('/reviews', shopAuth, async (req: Request, res: Response) => {
+  try {
+    const shopID: string = res.locals.user.id
+    const service = new SequelizeShopReviewsFetchingService(sequelize)
+    const shopReviews = await service.getShopReviews(shopID)
+    res.status(200).json(response(shopReviews))
+  } catch (err) {
+    res.status(400).json(response(null, err as Error))
+  }
 })
 
 router.post('/withdraw-requests', shopAuth, async (req: Request, res: Response) => {
